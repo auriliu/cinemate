409fb9d7a25fa79427cfa6e74d1959b25d5f96c0
audesmus <aureliu.aurelius@gmail.com>
2023-12-07 22:31:28 +0100

    working files merged

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+// import AppV1 from "./AppV1";
+
+// import { StarRating } from "./components/StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -9,3 +12,5 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// OMDB API KEY db7c17fa
