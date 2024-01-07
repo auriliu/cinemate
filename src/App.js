2f427e90d09dd7071c7dca85f722700a0e49994e
audesmus <aureliu.aurelius@gmail.com>
2024-01-07 20:12:53 +0100

    updated and finished

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 
+import logo from "./logo.resized.png";
+
 // import { StarRating } from "./components/StarRating";
 
 // OMDB API KEY db7c17fa
@@ -107,8 +109,7 @@ function Navbar({ children }) {
 function Logo() {
   return (
     <div className="logo">
-      <span role="img">🍿</span>
-      <h1>usePopcorn</h1>
+      <img src={logo} alt="" className="logo-img" />
     </div>
   );
 }
@@ -136,16 +137,7 @@ function Main({ children }) {
 }
 
 function Box({ children }) {
-  const [isOpen, setIsOpen] = useState(true);
-
-  return (
-    <div className="box">
-      <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
-        {isOpen ? "–" : "+"}
-      </button>
-      {isOpen && children}
-    </div>
-  );
+  return <div className="box">{children}</div>;
 }
 function MovieList({ movies, handleSelectedMovie }) {
   return (
@@ -175,68 +167,6 @@ function Movie({ movie, handleSelectedMovie }) {
     </li>
   );
 }
-
-// function WatchedSummary({ watched }) {
-//   const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
-//   const avgUserRating = average(watched.map((movie) => movie.userRating));
-//   const avgRuntime = average(watched.map((movie) => movie.runtime));
-
-//   return (
-//     <div className="summary">
-//       <h2>Movies you watched</h2>
-//       <div>
-//         <p>
-//           <span>#️⃣</span>
-//           <span>{watched.length} movies</span>
-//         </p>
-//         <p>
-//           <span>⭐️</span>
-//           <span>{avgImdbRating}</span>
-//         </p>
-//         <p>
-//           <span>🌟</span>
-//           <span>{avgUserRating}</span>
-//         </p>
-//         <p>
-//           <span>⏳</span>
-//           <span>{avgRuntime} min</span>
-//         </p>
-//       </div>
-//     </div>
-//   );
-// }
-// function WatchedMoviesList({ watched }) {
-//   return (
-//     <ul className="list">
-//       {watched.map((movie) => (
-//         <WatchedMovie key={movie.imdbID} movie={movie} />
-//       ))}
-//     </ul>
-//   );
-// }
-
-// function WatchedMovie({ movie }) {
-//   return (
-//     <li>
-//       <img src={movie.Poster} alt={`${movie.Title} poster`} />
-//       <h3>{movie.Title}</h3>
-//       <div>
-//         <p>
-//           <span>⭐️</span>
-//           <span>{movie.imdbRating}</span>
-//         </p>
-//         <p>
-//           <span>🌟</span>
-//           <span>{movie.userRating}</span>
-//         </p>
-//         <p>
-//           <span>⏳</span>
-//           <span>{movie.runtime} min</span>
-//         </p>
-//       </div>
-//     </li>
-//   );
-// }
 
 function MovieDetails({ selectedId, onCloseMovie }) {
   const [movie, setMovie] = useState({});
